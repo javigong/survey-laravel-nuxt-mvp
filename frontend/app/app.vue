8b0bf8330d09d80d190e5ab9e0a5d7b1184532b2
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-10-05 23:05:04 -0700

    Update app.vue

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -6,6 +6,10 @@
 </template>
 
 <script setup lang="ts">
+// Import CSS
+import '../assets/css/main.css'
+import '../assets/css/tailwind.css'
+
 // Initialize auth store on app start
 const authStore = useAuthStore()
 
