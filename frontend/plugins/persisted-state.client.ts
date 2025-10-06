d7a919cf756383b3892b2c9c8d4f55c334f3c2d6
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-10-05 23:05:23 -0700

    Update persisted-state.client.ts

--- frontend/plugins/persisted-state.client.ts
+++ frontend/plugins/persisted-state.client.ts
@@ -2,6 +2,23 @@ import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 export default defineNuxtPlugin(({ $pinia }) => {
   $pinia.use(createPersistedState({
-    storage: persistedState.localStorage,
+    storage: {
+      getItem: (key: string) => {
+        if (process.client) {
+          return localStorage.getItem(key)
+        }
+        return null
+      },
+      setItem: (key: string, value: string) => {
+        if (process.client) {
+          localStorage.setItem(key, value)
+        }
+      },
+      removeItem: (key: string) => {
+        if (process.client) {
+          localStorage.removeItem(key)
+        }
+      },
+    },
   }))
 })
