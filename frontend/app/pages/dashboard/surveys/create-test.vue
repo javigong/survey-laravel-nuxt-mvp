6e24dcd4939b4d79be0f81451e4a95b35bc939d5
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-10-06 21:42:42 -0700

    Create create-test.vue

---
+++ frontend/app/pages/dashboard/surveys/create-test.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Test Create Page</h1>
+    <p>This is a test page to see if routing works.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+console.log("Test create page loaded");
+</script>
