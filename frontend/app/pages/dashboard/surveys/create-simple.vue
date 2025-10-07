5c82ec41c8fbd571b8c4c368b28a0c65534dcc49
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-10-06 21:42:41 -0700

    Create create-simple.vue

---
+++ frontend/app/pages/dashboard/surveys/create-simple.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Create Survey - Simple Test</h1>
+    <p>This is a simple test page to verify routing works.</p>
+    <NuxtLink to="/dashboard/surveys">Back to Surveys</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+console.log("Simple create page loaded");
+</script>
