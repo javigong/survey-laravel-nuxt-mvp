9e8431b34d65d6ceffeef5bc03917f77a2c846f1
Javier Gongora <42308135+javigong@users.noreply.github.com>
2025-10-06 21:42:36 -0700

    Update app.vue

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
 
 <script setup lang="ts">
 // Import CSS
-import '../assets/css/main.css'
-import '../assets/css/tailwind.css'
+import "../assets/css/main.css";
+import "../assets/css/tailwind.css";
 
 // Initialize auth store on app start
-const authStore = useAuthStore()
+const authStore = useAuthStore();
 
 // Initialize authentication state from localStorage
 onMounted(() => {
-  authStore.initializeAuth()
-})
+  authStore.initializeAuth();
+});
 </script>
